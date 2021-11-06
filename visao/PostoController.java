20606363ba09de9ce89ed1795a4a7322ed984b00
Gustheou <gustavopn117@gmail.com>
2021-11-06 18:01:01 -0300

    Atualização

--- visao/PostoController.java
+++ visao/PostoController.java
@@ -1,6 +1,51 @@
 package visao;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
 
 public class PostoController {
+
+    @FXML
+    private TextField numeroDeLinhasTextField;
+
+    @FXML
+    private TextField numeroDeColunasTextField;
+
+    @FXML
+    private Button calcularButton;
+    
+    @FXML
+    private GridPane gridPane;
+
+
+    @FXML
+    void mostrarMatrizButton(ActionEvent event) {
+        try {
+            int numeroDeLinhas =  Integer.parseInt(numeroDeLinhasTextField.getText());
+            int numeroDeColuna = Integer.parseInt(numeroDeColunasTextField.getText());
+            GridPane.setColumnIndex(gridPane, numeroDeColuna);
+            GridPane.setRowIndex(gridPane, numeroDeLinhas);
+            for (int i = 0; i < numeroDeLinhas; ++i) {
+                for (int j = 0; j < numeroDeColuna; ++j) {
+                    gridPane.add(new TextField(), j, i);
+                   
+                }
+            }
+            calcularButton.setVisible(true);
+            
+            
+        } catch (NumberFormatException n) {
+            //TODO: handle exception
+        }
+    }
+
+    @FXML
+    void calcularPostoDeMatriz(ActionEvent event) {
+
+    }
+    
 
 }
